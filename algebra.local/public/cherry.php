3f1822fb2f2bac0f764dcdf3c2c834659615bbdd
Stipan <stipan.dizdar@gmail.com>
2023-12-10 13:33:16 +0100

    iinicijalni commit

--- algebra.local/public/cherry.php
+++
@@ -1,7 +0,0 @@
-<?php
-$DBconnect=mysqli_connect("localhost", "phpUser", "php", "testDB");
-$result = mysqli_query($DBconnect, "SELECT * FROM breakfastMenu");
-while($row = mysqli_fetch_array($result))
-	print_r($row['food']);
-mysqli_close($DBconnect);
-?>
